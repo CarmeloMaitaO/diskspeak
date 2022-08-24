6f39f6353c9216b91ae00efc0af4292cd683bdbd
CarmeloMaitaO <98987672+CarmeloMaitaO@users.noreply.github.com>
2022-08-24 08:58:28 -0400

    Creada la interfaz de línea de comandos

--- main.cpp
+++ main.cpp
@@ -1,1 +1,142 @@
+#include <unistd.h>
+#include <cstring>
 
+void help();
+
+void version();
+
+void test(char* arg);
+
+bool check(char* arg);
+
+void parser(char* arg);
+
+void read();
+
+void sread();
+
+void write();
+
+void swrite();
+
+int main(int argc, char** argv)
+{
+        for(;;)
+        {
+                switch(getopt(argc, argv, "p:vh"))
+                {
+                        case 'h':
+                                help();
+                                break;
+                        case 'v':
+                                version();
+                                break;
+                        case 'p':
+                                test(optarg);
+                                break;
+                        default:
+                                help();
+                                break;
+                }
+                break;
+        }
+        return 0;
+}
+
+void help()
+{
+        std::cout << "Instructions:" << std::endl;
+        std::cout << "diskspeak -h|v" << std::endl;
+        std::cout << "diskspeak -p \'[rw]...\'" << std::endl;
+}
+
+void version()
+{
+        std::cout << "Program version: Diskspeak 1.0";
+}
+
+void test(char* arg)
+{
+        if (check(arg))
+        {
+                parser(arg);
+        } else {
+                help();
+        }
+}
+
+bool check(char* arg)
+{
+        bool error = false;
+        if (strlen(arg)>0)
+        {
+                for (int counter = 0; counter<strlen(arg) && !error; counter++)
+                {
+                        switch (arg[counter])
+                        {
+                                case 'R':
+                                        break;
+                                case 'r':
+                                        break;
+                                case 'W':
+                                        break;
+                                case 'w':
+                                        break;
+                                default:
+                                        error = true;
+                        }
+                }
+        } else
+        {
+                error = true;
+        }
+        if (error)
+        {
+                return false;
+        } else
+        {
+                return true;
+        }
+}
+
+void parser(char* arg)
+{
+        for (int counter = 0; counter<strlen(arg); counter++)
+        {
+                switch (arg[counter])
+                {
+                        case 'R':
+                                read();
+                                break;
+                        case 'r':
+                                sread();
+                                break;
+                        case 'W':
+                                write();
+                                break;
+                        case 'w':
+                                swrite();
+                                break;
+                }
+        }
+}
+
+void read()
+{
+  std::cout << "Lectura" << std::endl;
+}
+
+void sread()
+{
+  std::cout << "Lectura inteligente" << std::endl;
+}
+
+void write()
+{
+  std::cout << "Escritura" << std::endl;
+}
+
+void swrite()
+{
+  std::cout << "Escritura inteligente" << std::endl;
+}
